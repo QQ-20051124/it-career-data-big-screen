c5fa6c54e4ff3fe2e24a51e2c92313bc33a8342c
maishuqing <3024575887@qq.com>
2026-07-07 01:14:27 +0800

    12

--- my-vue3/src/router/index.js
+++ my-vue3/src/router/index.js
@@ -3,6 +3,7 @@ import BigScreen from '@/components/BigScreen.vue'
 import LoginView from '@/components/LoginView.vue'
 import Dashboard from '@/components/Dashboard.vue'
 import JobRecommend from '@/components/JobRecommend.vue'
+import AIResume from '@/components/AIResume.vue'
 
 const routes = [
   {
@@ -24,6 +25,11 @@ const routes = [
     path: '/job-recommend',
     name: 'JobRecommend',
     component: JobRecommend
+  },
+  {
+    path: '/ai-resume',
+    name: 'AIResume',
+    component: AIResume
   }
 ]
 
